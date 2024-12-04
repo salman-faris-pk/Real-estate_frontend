c1bc7f97ca8a5aeb86601966f6507c719aa9e3b6
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-12-04 22:41:06 +0530

    single page almost done...

--- src/pages/SinglePage.tsx
+++ src/pages/SinglePage.tsx
@@ -4,108 +4,110 @@ import { Map } from "../components/Map"
 export const SinglePage = () => {
   return (
     
-    <div className="singlePage">
+    <div className="flex-col md:flex-row flex overflow-scroll thin-scrollbar md:overflow-hidden h-full ">
       {/**left side */}
-    <div className="details">
-      <div className="wrapper">
+    <div className="flex-none h-max mb-[50px] md:mb-[0px] md:flex-[3] md:h-full overflow-y-scroll thin-scrollbar">
+      <div className="pr-[50px] sm:pr-[0px] md:pr-[20px]">
         {/* <Slider images={singlePostData.images} /> */}
-        <div className="info">
-          <div className="top">
-            <div className="post">
-              <h1>{singlePostData.title}</h1>
-              <div className="address">
-                <img src="/pin.png" alt="" />
+        <div className="mt-[50px]">
+          <div className="flex-col gap-[20px] sm:gap-[0px] sm:flex-row flex justify-between">
+            <div className="flex flex-col gap-[20px]">
+              <h1 className="font-semibold text-[17px] md:text-[20px]">{singlePostData.title}</h1>
+              <div className="flex gap-[5px] items-center text-[#888] text-[12px]">
+                <img src="/pin.png" alt="pin" className="w-4 h-4" />
                 <span>{singlePostData.address}</span>
               </div>
-              <div className="price">$ {singlePostData.price}</div>
+              <div className="p-[5px] bg-[rgba(254,205,81,0.438)]  rounded-[5px] w-max text-[16px] font-light">
+                $ {singlePostData.price}
+              </div>
             </div>
-            <div className="user">
-              <img src={userData.img} alt="" />
+            <div className="flex flex-col items-center justify-center gap-[20px] px-[50px] py-0 sm:py-[20px] rounded-[10px] bg-[rgba(254,205,81,0.209)] text-sm font-semibold">
+              <img src={userData.img} alt="user" className="w-[50px] h-[50px] rounded-full object-cover" />
               <span>{userData.name}</span>
             </div>
           </div>
-          <div className="bottom">{singlePostData.description}</div>
+          <div className="mt-[50px] text-sm text-[#555] leading-5">{singlePostData.description}</div>
         </div>
       </div>
     </div>
-    
+
     {/**right side */}
-    <div className="features">
-      <div className="wrapper">
-        <p className="title">General</p>
-        <div className="listVertical">
+    <div className="flex-none md:flex-[2] bg-[#fcf5f3] h-max mb-[50px] md:mb-[0px]  md:h-full overflow-scroll thin-scrollbar">
+      <div className="md:py-0 md:px-[20px] p-[20px] flex flex-col gap-[20px]">
+        <p className="font-bold text-[17px] mb-[10px]">General</p>
+        <div className="flex flex-col gap-[20px] py-[20px] px-[10px] bg-white rounded-[10px]">
           <div className="feature">
-            <img src="/utility.png" alt="" />
+            <img src="/utility.png" alt="utility" className="w-6 h-6 bg-[rgba(254,205,81,0.209)]" />
             <div className="featureText">
-              <span>Utilities</span>
-              <p>Renter is responsible</p>
+              <span className="font-semibold text-sm">Utilities</span>
+              <p className="text-[10px]">Renter is responsible</p>
             </div>
           </div>
           <div className="feature">
-            <img src="/pet.png" alt="" />
-            <div className="featureText">
-              <span>Pet Policy</span>
-              <p>Pets Allowed</p>
+            <img src="/pet.png" alt="pet" className="w-6 h-6 bg-[rgba(254,205,81,0.209)]" />
+            <div>
+              <span className="font-semibold text-sm">Pet Policy</span>
+              <p className="text-[10px]">Pets Allowed</p>
             </div>
           </div>
           <div className="feature">
-            <img src="/fee.png" alt="" />
-            <div className="featureText">
-              <span>Property Fees</span>
-              <p>Must have 3x the rent in total household income</p>
+            <img src="/fee.png" alt="fee" className="w-6 h-6 bg-[rgba(254,205,81,0.209)]"/>
+            <div>
+              <span className="font-semibold text-sm">Property Fees</span>
+              <p className="text-[10px]">Must have 3x the rent in total household income</p>
             </div>
           </div>
         </div>
-        <p className="title">Sizes</p>
-        <div className="sizes">
-          <div className="size">
-            <img src="/size.png" alt="" />
+        <p className="font-bold text-[17px] mb-[10px]">Sizes</p>
+        <div className="flex justify-between text-xs md:text-[12px]">
+          <div className="flex items-center gap-[10px] bg-white p-[10px] rounded-[5px]">
+            <img src="/size.png" alt="size" className="w-5 h-5"/>
             <span>80 sqft</span>
           </div>
-          <div className="size">
-            <img src="/bed.png" alt="" />
+          <div className="flex items-center gap-[10px] bg-white p-[10px] rounded-[5px]">
+            <img src="/bed.png" alt="bed" className="w-6 h-6"/>
             <span>2 beds</span>
           </div>
-          <div className="size">
-            <img src="/bath.png" alt="" />
+          <div className="flex items-center gap-[10px] bg-white p-[10px] rounded-[5px]">
+            <img src="/bath.png" alt="bathtub" className="w-6 h-6"/>
             <span>1 bathroom</span>
           </div>
         </div>
-        <p className="title">Nearby Places</p>
-        <div className="listHorizontal">
+        <p className="font-bold text-[17px] mb-[10px]">Nearby Places</p>
+        <div className="flex justify-between py-[20px] px-[10px] bg-white rounded-[10px]">
           <div className="feature">
-            <img src="/school.png" alt="" />
-            <div className="featureText">
-              <span>School</span>
-              <p>250m away</p>
+            <img src="/school.png" alt=" school" className="w-6 h-6 bg-[rgba(254,205,81,0.209)]" />
+            <div>
+              <span className="font-bold text-xs sm:text-sm">School</span>
+              <p className="text-[9px] md:text-[12px]">250m away</p>
             </div>
           </div>
           <div className="feature">
-            <img src="/pet.png" alt="" />
-            <div className="featureText">
-              <span>Bus Stop</span>
-              <p>100m away</p>
+            <img src="/pet.png" alt="pet" className="w-6 h-6 bg-[rgba(254,205,81,0.209)]" />
+            <div>
+              <span className="font-bold text-xs sm:text-sm">Bus Stop</span>
+              <p className="text-[9px] md:text-[12px]">100m away</p>
             </div>
           </div>
           <div className="feature">
-            <img src="/fee.png" alt="" />
-            <div className="featureText">
-              <span>Restaurant</span>
-              <p>200m away</p>
+            <img src="/fee.png" alt="fee" className="w-6 h-6 bg-[rgba(254,205,81,0.209)]"/>
+            <div>
+              <span className="font-bold text-xs sm:text-sm">Restaurant</span>
+              <p className=" text-[9px] md:text-[12px]">200m away</p>
             </div>
           </div>
         </div>
-        <p className="title">Location</p>
-        <div className="mapContainer">
+        <p className="font-bold text-[17px] mb-[10px]">Location</p>
+        <div className="w-full h-[200px]">
           <Map items={listData} />
         </div>
-        <div className="buttons">
-          <button>
-            <img src="/chat.png" alt="" />
+        <div className="flex justify-between">
+          <button className="p-4 flex items-center gap-[5px] text-xs bg-white border border-solid border-[#fece51] rounded-[5px] cursor-pointer">
+            <img src="/chat.png" alt="chat" className="w-4 h-4"/>
             Send a Message
           </button>
-          <button>
-            <img src="/save.png" alt="" />
+          <button className="p-4 flex items-center gap-[5px] text-xs bg-white border border-solid border-[#fece51] rounded-[5px] cursor-pointer">
+            <img src="/save.png" alt="save" className="w-4 h-4"/>
             Save the Place
           </button>
         </div>
